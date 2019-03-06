7ec3cae3bd2904deb63c4c91409b8f73223b5836
Cindy Xue <cindyxue1405@gmail.com>
2019-03-05 20:07:21 -0800

    Update header

--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -32,7 +32,7 @@ function setPageTitle() {
 /**
  * Shows the message form if the user is logged in and viewing their own page.
  */
-function showMessageFormIfLoggedIn() {
+function showMessageFormIfViewingSelf() {
   document.getElementById('about-me-form').classList.remove('hidden');
 
   fetch('/login-status')
