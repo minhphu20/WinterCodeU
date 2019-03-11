afe9f8884b6286e297b1a254a9da6eb6bbb92a01
Rafael Hernandez <rafaelhernandez7232@gmail.com>
2019-03-10 23:24:15 -0700

    initial images code with regex

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -76,12 +76,20 @@ public class MessageServlet extends HttpServlet {
     }
 
     String user = userService.getCurrentUser().getEmail();
-    String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+    String userText = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+
+    String regex = "(https?://\\S+\\.(png|jpg))";
+    String replacement = "<img src=\"$1\" />";
+    String textWithImagesReplaced = userText.replaceAll(regex, replacement);
+    //
+    // Message message = new Message(user, textWithImagesReplaced);
+    // datastore.storeMessage(message);
+
     String recipient = request.getParameter("recipient");
 
-    Message message = new Message(user, text, recipient);
+    Message message = new Message(user, textWithImagesReplaced, recipient);
     datastore.storeMessage(message);
-    
+
     response.sendRedirect("/user-page.html?user=" + recipient);
 
   }
