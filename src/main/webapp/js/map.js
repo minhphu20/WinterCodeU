cf9e054818e20bec5b454efb0249078585e9f206
Cindy Xue <cindyxue1405@gmail.com>
2019-03-11 15:43:36 -0700

    Modified based on comments

--- src/main/webapp/js/map.js
+++ src/main/webapp/js/map.js
@@ -1,4 +1,8 @@
       let map;
+
+      /**
+       * Build the map
+       */
       function createMap(){
           map = new google.maps.Map(document.getElementById('map'), {
             center: {lat: 37.422, lng: -122.084},
@@ -6,30 +10,33 @@
           });
 
           // Add a marker at Googleplex
-          const trexMarker = new google.maps.Marker({
-            position: {lat: 37.421903, lng: -122.084674},
-            map: map,
-            title: 'Stan the T-Rex',
-          });
-          var trexInfoWindow = new google.maps.InfoWindow({
-              content: 'This is Stan, the T-Rex statue.'
-          })
-          trexMarker.addListener('click', function() {
-            trexInfoWindow.open(map, trexMarker);
-          });
+          addMarker('Stan the T-Rex', 37.421903, -122.084674, null, 'This is Stan, the T-Rex statue.', map);
 
           // Add a marker for the dog park
           var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-          const parkMarker = new google.maps.Marker({
-              position: {lat: 37.428352, lng: -122.077574},
-              map: map,
-              title: 'Dog Park',
-              icon: image
-          })
-          var parkInfoWindow = new google.maps.InfoWindow({
-              content: 'This is a park for dogs.'
-          })
-          parkMarker.addListener('click', function() {
-            parkInfoWindow.open(map, parkMarker);
-          });
+          addMarker('Stan the T-Rex', 37.428352, -122.077574, image, 'This is a park for dogs.', map);
+      }
+
+      /**
+       * Add markers to the map
+       * @param {*} title title of the marker
+       * @param {*} lat latitude of th marker
+       * @param {*} lng longitude of the marker
+       * @param {*} image image of the marker, null for default image
+       * @param {*} content content of the slide info window
+       * @param {*} map map
+       */
+      function addMarker(title, lat, lng, image, content, map) {
+        const marker = new google.maps.Marker({
+          position: {lat: lat, lng: lng},
+          map: map,
+          title: title,
+          icon: image
+        });
+        var infoWindow = new google.maps.InfoWindow({
+            content: content
+        })
+        marker.addListener('click', function() {
+          infoWindow.open(map, marker);
+        });
       }
