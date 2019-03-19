9b6a66ce65e2087f04beca66b30e8d493417f59e
Cindy Xue <cindyxue1405@gmail.com>
2019-03-18 21:57:08 -0700

    Add marker clustering

--- src/main/webapp/js/ufo.js
+++ src/main/webapp/js/ufo.js
@@ -7,22 +7,36 @@ function createUfoSightingsMap(){
       return response.json();
     }).then((ufoSightings) => {
         
+      // Construct a map
       const map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 35.78613674, lng: -119.4491591},
         zoom:7
       });
 
+      var markers = [];
+
       ufoSightings.forEach((ufoSighting) => {
+        // Construct a marker for each ufoSighting
         const marker = new google.maps.Marker({
           position: {lat: ufoSighting.lat, lng: ufoSighting.lng},
           map: map,
         });
+        // Add it into the markers array
+        markers.push(marker);
+
+        // Add info windows
         var infoWindow = new google.maps.InfoWindow({
           content: ufoSighting.state
         })
+
+        // Display infoWindow if clicks on the marker
         marker.addListener('click', function() {
           infoWindow.open(map, marker);
         });
       });
+
+      // Add a marker clusterer to manage the markers.
+      var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
     });
-  }
+}
