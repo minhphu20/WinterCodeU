c1d5fe65168242a6c45bd3895e23c9b97251b856
Cindy Xue <cindyxue1405@gmail.com>
2019-03-23 17:17:14 -0700

    Add INFO WINDOW CONTENT

--- src/main/webapp/js/user-map.js
+++ src/main/webapp/js/user-map.js
@@ -1,7 +1,9 @@
 let map;
 
+/**
+ * Create a map
+ */
 function createMap(){
-
     map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 38.5949, lng: -94.8923},
     zoom: 4
@@ -13,9 +15,42 @@ function createMap(){
     });
 
     map.addListener('click', (event) => {
-        new google.maps.Marker({
-          position: event.latLng,
-          map: map
-        });  
-      });
+        const clickLatLng = event.latLng
+        createMarkerForEdit(clickLatLng.lat(), clickLatLng.lng());
+    });
 }
+
+/**
+ * Populates the content in an Info Window that we attach to our marker
+ * @param {*} map map
+ * @param {*} lat latitude
+ * @param {*} lng longitude
+ */
+function createMarkerForEdit(lat, lng){
+    const marker = new google.maps.Marker({
+        position: {lat: lat, lng: lng},
+        map: map
+    });  
+            
+    const infoWindow = new google.maps.InfoWindow({
+        content: buildInfoWindowInput()
+    });
+        
+    infoWindow.open(map, marker);
+}
+
+/**
+ * Builds a div that contains a textarea and a button.
+ */
+function buildInfoWindowInput(){
+    const textBox = document.createElement('textarea');
+    const button = document.createElement('button');
+    button.appendChild(document.createTextNode('Submit'));
+       
+    const containerDiv = document.createElement('div');
+    containerDiv.appendChild(textBox);
+    containerDiv.appendChild(document.createElement('br'));
+    containerDiv.appendChild(button);
+       
+    return containerDiv;
+  }
