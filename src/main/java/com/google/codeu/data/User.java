c0cfe45d0d11cf411dbe3eefc22ef8ba3043349e
Jenny Chau <jchau9@uci.edu>
2019-04-09 22:19:52 -0700

    starting profile

--- src/main/java/com/google/codeu/data/User.java
+++ src/main/java/com/google/codeu/data/User.java
@@ -3,11 +3,21 @@ package com.google.codeu.data;
 public class User {
 
   private String email;
+  private String name;
+  private String breed;
+  private String gender;
+  private String birthday;
   private String aboutMe;
+  private String weight;
 
-  public User(String email, String aboutMe) {
+  public User(String email, String aboutMe, String name, String breed, String gender, String birthday, String weight) {
     this.email = email.trim();
     this.aboutMe = aboutMe;
+    this.name = name;
+    this.breed = breed;
+    this.gender = gender;
+    this.birthday = birthday;
+    this.weight = weight;
   }
 
   public String getEmail() {
@@ -16,5 +26,25 @@ public class User {
 
   public String getAboutMe() {
     return aboutMe;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public String getBreed() {
+    return breed;
+  }
+
+  public String getGender() {
+    return gender;
+  }
+
+  public String getBirthday() {
+    return birthday;
+  }
+
+  public String getWeight() {
+    return weight;
   }
 }
