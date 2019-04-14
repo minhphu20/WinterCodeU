10c61b994f848781da58fbace80d1ddc52e4b863
minhphu2020 <741redwood@gmail.com>
2019-04-14 00:33:09 -0700

    everything working, not pop up chat though

---
+++ src/main/java/com/google/codeu/servlets/LikeServlet.java
@@ -0,0 +1,82 @@
+package com.google.codeu.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import javax.servlet.ServletContext;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import com.google.codeu.data.User;
+
+/** Handles fetching and saving likes and not likes. */
+@WebServlet("/like")
+public class LikeServlet extends HttpServlet {
+
+    private Datastore datastore;
+
+    @Override
+    public void init() {
+        datastore = new Datastore();
+    }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        System.out.println("Posting like in servlet...");
+
+        UserService userService = UserServiceFactory.getUserService();
+
+        String userEmail = userService.getCurrentUser().getEmail();
+
+        User user = datastore.getUser(userEmail);
+        // Note: sometimes the current user is not stored in the datastore!
+        if (user == null) {
+            System.out.println("The current user is not in Datastore. Adding them...");
+            user = new User(userEmail, null, new HashSet<String>(), new HashSet<String>());
+        }
+
+        // Get who the user likes
+        String prospectiveEmail = request.getParameter("email");
+        System.out.println("prospective email: " + prospectiveEmail);
+        User prospectiveMatch = datastore.getUser(prospectiveEmail);
+        // Note: sometimes the prospective user is not stored in the datastore!
+        if (prospectiveMatch == null) {
+            System.out.println("The prospective user is not in Datastore. Adding them...");
+            prospectiveMatch = new User(prospectiveEmail, null, new HashSet<String>(), new HashSet<String>());
+        }
+
+        String status = request.getParameter("status");
+        System.out.println("status: " + status);
+        if ("like".equals(status)) {
+            user.addLike(prospectiveEmail);
+            System.out.println("Done adding like...");
+        } else {
+            user.addNotLike(prospectiveEmail);
+            System.out.println("Done adding not like...");
+        }
+
+        datastore.storeUser(user);
+
+        // Hey CINDY :) This is where you can start modifying
+        // stuffs to make the chat happen!!
+
+        // yes means 2 users like each other, no means 1 of them does not like the other.
+        if (("like".equals(status)) && (datastore.isLiked(user, prospectiveMatch))) {
+            System.out.println("It's a match! We will start chatting...");
+            response.setContentType("text/html");
+            response.getWriter().println("yes");
+        } else {
+            response.setContentType("text/html");
+            response.getWriter().println("no");
+        }
+    }
+}
--- src/main/webapp/js/swipe-loader.js
+++ src/main/webapp/js/swipe-loader.js
@@ -1,6 +1,4 @@
-/**
- * Redirect to homepage if the user is not logged in.
- */
+/** Redirect to homepage if the user is not logged in. */
 function showMessageFormIfLoggedIn() {
     fetch('/login-status')
     .then((response) => {
@@ -22,7 +20,7 @@ function fetchProspect() {
         })
         .then((user) => {
             if (user['email']) {
-                document.getElementById('prospect_name').innerText = user['email'];
+                document.getElementById('prospect_email').innerText = user['email'];
                 // TODO: add image profile of user -> waiting on Jenny to make profile picture compulsory
                 // Right now we have a very cute placeholder for the image
                 document.getElementById('prospect_image').src = "images/doge.jpg";
@@ -34,7 +32,7 @@ function fetchProspect() {
 
             } else {
                 // TODO: add hidden class to properly hide things
-                document.getElementById('prospect_name').innerText = "You have seen all users."
+                document.getElementById('prospect_email').innerText = "You have seen all users."
                 document.getElementById('prospect_image').src = "images/profEample.jpg";
                 document.getElementById('prospect_aboutMe').innerText = "";
                 document.getElementById('buttons').innerText = "";
@@ -42,7 +40,48 @@ function fetchProspect() {
         })
 }
 
-/** Load the image page with image fetched from /prospect. */
+/** Posts like. */
+function postLike() {
+    console.log("liking...");
+    let targetEmail = document.getElementById('prospect_email').innerHTML;
+    fetch("/like?email=" + targetEmail + "&status=like", { method: "POST" })
+            // Hey CINDY :) This is where you can start modifying
+            // stuffs to make the chat happen!!
+        .then((response) => {
+            return response.text();
+        })
+        .then((answer) => {
+            console.log("answer is: ", answer);
+            if (answer === "yes") {
+                // Hey Cindy, you can open chat page here.
+                //
+                // Right now this check answer === "yes"
+                // will returns false all the time
+                // because answer is a promise...
+                // Please look into that (async, then may help) :) Thanks!
+                // Otherwise, everything should be working.
+                console.log("Chat page should open!");
+            } else {
+                console.log("No chat page!");
+                fetchProspect();
+            }
+        })
+}
+
+/** Posts not like. */
+function postNotLike() {
+    console.log("not liking...");
+    let targetEmail = document.getElementById('prospect_email').innerHTML;
+    fetch("/like?email=" + targetEmail + "&status=notlike", { method: "POST" })
+        .then((response) => {
+            return response.text();
+        })
+        .then((answer) => {
+            fetchProspect();
+        })
+}
+
+/** Loads the image page with image fetched from /prospect. */
 function loadProspect() {
     showMessageFormIfLoggedIn();
     fetchProspect();
