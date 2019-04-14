121e4613a7491b09d7ddde6a33a5a1400f8c9acf
minhphu2020 <741redwood@gmail.com>
2019-04-13 21:25:56 -0700

    Add swipe page loader, load image

--- src/main/webapp/js/swipe-loader.js
+++ src/main/webapp/js/swipe-loader.js
@@ -13,4 +13,20 @@ function showMessageFormIfLoggedIn() {
     });
 }
 
+/** Fetches a prospect and add their info (name, image, aboutMe) to the page. */
+function fetchProspect() {
+    const url = '/messages?user=' + 'test@example.com';
+    fetch(url)
+        .then((response) => {
+            return response.json();
+        })
+        .then((user) => {
+            document.getElementById('prospect_name').innerText = "jalala@example.com";
+            document.getElementById('prospect_image').src = "images/doge.jpg";
+            document.getElementById('prospect_aboutMe').innerText = "I adore running outside.";
+
+        })
+}
+
 showMessageFormIfLoggedIn();
+fetchProspect();
