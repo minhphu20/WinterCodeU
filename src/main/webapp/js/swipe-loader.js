6dd4f4e2ea6fec605ce51420022c882ebf6501a0
minhphu2020 <741redwood@gmail.com>
2019-04-13 23:21:25 -0700

    Add Prospect Servlet

--- src/main/webapp/js/swipe-loader.js
+++ src/main/webapp/js/swipe-loader.js
@@ -16,18 +16,29 @@ function showMessageFormIfLoggedIn() {
 /** Fetches a prospect and add their info (name, image, aboutMe) to the page. */
 // NOTE: right now it is a place holder
 function fetchProspect() {
-    const url = '/messages?user=' + 'test@example.com';
+    const url = '/prospect';
     fetch(url)
         .then((response) => {
             return response.json();
         })
         .then((user) => {
-            if (user) {
-                document.getElementById('prospect_name').innerText = "jalala@example.com";
+            if (user['email']) {
+                document.getElementById('prospect_name').innerText = user['email'];
+                // TODO: add image profile of user -> waiting on Jenny to make profile picture compulsory
+                // Right now we have a very cute placeholder for the image
                 document.getElementById('prospect_image').src = "images/doge.jpg";
-                document.getElementById('prospect_aboutMe').innerText = "I adore running outside.";
+                if (user['aboutMe']) {
+                    document.getElementById('prospect_aboutMe').innerText = user['aboutMe'];
+                } else {
+                    document.getElementById('prospect_aboutMe').innerText = "I have not add an about me ;)";
+                }
+
             } else {
-                document.getElementById('prospect').innerText = "You have seen all our users.";
+                // TODO: add hidden class to properly hide things
+                document.getElementById('prospect_name').innerText = "You have seen all users."
+                                document.getElementById('prospect_image').src = "images/profEample.jpg";
+                                document.getElementById('prospect_aboutMe').innerText = "";
+                document.getElementById('buttons').innerText = "";
             }
         })
 }
