1fdf1e4ed554551b9fbf2d2c342f64cf08f4ff82
Cindy Xue <cindyxue1405@gmail.com>
2019-04-15 17:33:12 -0700

    Enable users to add markers

--- src/main/webapp/js/profile-map.js
+++ src/main/webapp/js/profile-map.js
@@ -1,4 +1,5 @@
 let map;
+let editMarker;
 
 /**
  * Build the map
@@ -25,9 +26,17 @@ function createMap(){
         addMarker('location', pos.lat, pos.lng, null, 'I\'m here', map);
 
         map.setCenter(pos);
+
+        map.addListener('click', (event) => {
+            createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+        });
+    
+        fetchMarkers();
+
       }, function() {
         handleLocationError(true, infoWindow, map.getCenter());
       });
+
     } else {
 
       map = new google.maps.Map(document.getElementById('map'), {
@@ -37,14 +46,19 @@ function createMap(){
 
       // Browser doesn't support Geolocation
       handleLocationError(false, infoWindow, map.getCenter());
+
+      map.addListener('click', (event) => {
+        createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+      });
+
+      fetchMarkers();
     }
 
-    // Add a marker at Googleplex
-    // addMarker('Stan the T-Rex', 37.421903, -122.084674, null, 'This is Stan, the T-Rex statue.', map);
+    // map.addListener('click', (event) => {
+    //     createMarkerForEdit(event.latLng.lat(), event.latLng.lng());
+    // });
 
-    // Add a marker for the dog park
-    var image = 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png';
-    // addMarker('Stan the T-Rex', 37.428352, -122.077574, image, 'This is a park for dogs.', map);
+    // fetchMarkers();
 }
 
 /**
@@ -70,3 +84,106 @@ function addMarker(title, lat, lng, image, content, map) {
     infoWindow.open(map, marker);
   });
 }
+
+/**
+ * Loads the markers
+ */
+function fetchMarkers(){
+    fetch('/user-markers').then((response) => {
+      return response.json();
+    }).then((markers) => {
+      markers.forEach((marker) => {
+        createMarkerForDisplay(marker.lat, marker.lng, marker.content)
+      });  
+    });
+}
+
+/**
+ * Populates the content in an Info Window that we attach to our marker
+ * @param {*} map map
+ * @param {*} lat latitude
+ * @param {*} lng longitude
+ */
+function createMarkerForEdit(lat, lng){
+    if(editMarker){
+        editMarker.setMap(null)
+    }
+
+    editMarker = new google.maps.Marker({
+        position: {lat: lat, lng: lng},
+        map: map
+    });  
+            
+    const infoWindow = new google.maps.InfoWindow({
+        content: buildInfoWindowInput(lat, lng)
+    });
+        
+    google.maps.event.addListener(infoWindow, 'closeclick', () => {
+        editMarker.setMap(null);
+    });
+
+    infoWindow.open(map, editMarker);
+}
+
+/**
+ * Builds a div that contains a textarea and a button.
+ * @param {*} map map
+ * @param {*} lat latitude
+ */
+function buildInfoWindowInput(lat, lng){
+    const textBox = document.createElement('textarea');
+    const button = document.createElement('button');
+    button.appendChild(document.createTextNode('Submit'));
+
+    button.onclick = () => {
+        postMarker(lat, lng, textBox.value);
+        createMarkerForDisplay(lat, lng, textBox.value);
+        editMarker.setMap(null);
+    }
+       
+    const containerDiv = document.createElement('div');
+    containerDiv.appendChild(textBox);
+    containerDiv.appendChild(document.createElement('br'));
+    containerDiv.appendChild(button);
+       
+    return containerDiv;
+  }
+
+  /**
+   * Adds the user's data to the map when they click the Submit button.
+   * @param {*} lat latitude
+   * @param {*} lng longitude
+   * @param {*} content user input form the textbox
+   */
+  function createMarkerForDisplay(lat, lng, content){
+    const marker = new google.maps.Marker({
+      position: {lat: lat, lng: lng},
+      map: map
+    });
+                
+    var infoWindow = new google.maps.InfoWindow({
+      content: content
+    });
+
+    marker.addListener('click', () => {
+      infoWindow.open(map, marker);
+    });
+  }
+
+  /**
+   * Post markers
+   * @param {*} lat latitude
+   * @param {*} lng longitude
+   * @param {*} content content
+   */
+  function postMarker(lat, lng, content){
+    const params = new URLSearchParams();
+    params.append('lat', lat);
+    params.append('lng', lng);
+    params.append('content', content);
+  
+    fetch('/user-markers', {
+      method: 'POST',
+      body: params
+    });
+  }
