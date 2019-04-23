eef74e158485b9d643f2d2b6d81550b980e41412
Jenny Chau <jchau9@uci.edu>
2019-04-23 16:41:44 -0700

    style changes

--- src/main/java/com/google/codeu/servlets/ProfileServlet.java
+++ src/main/java/com/google/codeu/servlets/ProfileServlet.java
@@ -69,7 +69,7 @@ public class ProfileServlet extends HttpServlet {
       address.add("");
       address.add("");
       address.add("");
-      userData = new User(user, "", new HashSet<String>(), new HashSet<String>(), "", "", "", "", "", address, "../images/cooldoge.png", "");
+      userData = new User(user, "", new HashSet<String>(), new HashSet<String>(), "", "", "", "", "", address, "../images/cooldoge.png", "doge, shiba inu, meme, cool");
       datastore.storeUser(userData);
     }
 
